8c35d02783a71fb18cb2c1fed7c7ef3ac49821d3
AkankshaSinghRajput <80374024+AkankshaSinghRajput@users.noreply.github.com>
2021-03-15 11:53:45 +0530

    Add files via upload

---
+++ DateClass.java
@@ -0,0 +1,10 @@
+
+public class DateClass {
+
+	public static void main(String[] args) {
+		Date date1 = new Date(2021, 3, 10);
+		
+
+	}
+
+}
---
+++ dataConversion.java
@@ -0,0 +1,17 @@
+
+public class dataConversion {
+	
+	public static void main(String[] args) {
+		int a = 129;
+		byte b = (byte) 129;
+		float f = 2.5f;
+		long l = 120000000000L;
+		double d = 2.59876543123456789;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(f);
+		System.out.println(l);
+		System.out.println(d);
+	}
+
+}
